07e25535b373047513ef26c5d567f2780dc93866
email2ify <email2ify@yahoo.com>
2022-12-06 11:11:56 +0000

    Add requirments for deployment

--- run.py
+++ run.py
@@ -1,3 +1,55 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Addition of two numbers
+def add(x, y):
+    return x + y
+
+# Subtraction of two numbers
+def subtract(x, y):
+    return x - y
+
+# Multiplication of two numbers
+def multiply(x, y):
+    return x * y
+
+
+# Division of two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+
+while True:
+    # User imput
+    choice = input("Enter choice(1/2/3/4): ")
+
+    # The four options for the user to addition, subtraction, multiplication and division
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        # To confirm if the user wants to do another calculation
+        # The loops breaks with if answer is no
+        next_calculation = input("Next calculation? (y/n): ")
+        if next_calculation == "n":
+            break
+
+    else:
+        print("Invalid Input")
